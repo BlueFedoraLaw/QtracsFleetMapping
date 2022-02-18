2eda9354b5cd90c2bd781ebd48026a59e3dc9d2b
Austin Morgan <amorgan@lawrencecompanies.com>
2022-02-18 08:56:46 -0500

    Updated Script names and added comments to displaycheck.sh

--- displayscripts/displaycheck.sh
+++ displayscripts/displaycheck.sh
@@ -1,7 +1,10 @@
+# checks for firefox display process
+# if process is there, exit with 0 status
+# if process is not there run the script that will initialize the displays
 ps -afx > processes.txt
 
 if ( grep "firefox" processes.txt ); then
 	:
 else
-	bash checkfordisplays.sh
+	bash init_displays.sh
 fi
